41ec6e4df722394fa4129e3a5618e1d5f2580766
Santthosh <santthosh@gmail.com>
2013-05-20 10:22:24 +0530

    Getting rid of the 'throw' action and focusing more on the 'frame' object

---
+++ Bowling/test/com/inmobi/bowling/TestFrame.java
@@ -0,0 +1,16 @@
+package com.inmobi.bowling;
+
+import junit.framework.TestCase;
+
+public class TestFrame extends TestCase {
+	
+	/**
+	 * A frame might provide its score, the players score at the current instance, 
+	 * number of pins in each throw, whether it was a strike or spare
+	 * 
+	 * @param name
+	 */
+	public TestFrame(String name) {
+		super(name);
+	}
+}
